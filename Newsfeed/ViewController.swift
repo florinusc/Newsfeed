c96dd28c193d303c46b07f553854a42925507deb
Florin Uscatu <florinusc@gmail.com>
2019-11-22 08:39:42 +0200

    Initial Commit

---
+++ Newsfeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newsfeed
+//
+//  Created by Florin Uscatu on 22/11/2019.
+//  Copyright © 2019 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
