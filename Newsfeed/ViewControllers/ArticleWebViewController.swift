f11da8dd9d68c6e483add231e2245aad219d10a1
Florin Uscatu <florinusc@gmail.com>
2019-11-23 19:14:17 +0200

    Add loader to web view

--- Newsfeed/ViewControllers/ArticleWebViewController.swift
+++ Newsfeed/ViewControllers/ArticleWebViewController.swift
@@ -16,6 +16,9 @@ class ArticleWebViewController: UIViewController {
     
     // MARK: - Public variables
     var viewModel: ArticleWebViewModel!
+
+    // MARK: - Private variables
+    private var loadingView: LoadingView?
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -25,13 +28,50 @@ class ArticleWebViewController: UIViewController {
     
     // MARK: - Private functions
     private func setup() {
+        setupLoader()
+        webView.navigationDelegate = self
         guard let request = viewModel.request else { return }
         webView.load(request)
+    }
+    
+    private func setupLoader() {
+        loadingView = .fromNib()
+        loadingView?.isHidden = true
+        loadingView?.frame = CGRect.zero
+        guard let loadingView = loadingView else { return }
+        view.addSubview(loadingView)
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+        loadingView.topAnchor.constraint(equalTo: webView.topAnchor).isActive = true
+        loadingView.bottomAnchor.constraint(equalTo: webView.bottomAnchor).isActive = true
+        loadingView.leftAnchor.constraint(equalTo: webView.leftAnchor).isActive = true
+        loadingView.rightAnchor.constraint(equalTo: webView.rightAnchor).isActive = true
+    }
+    
+    private func showLoader() {
+        loadingView?.isHidden = false
+    }
+    
+    private func hideLoader() {
+        loadingView?.isHidden = true
     }
     
     // MARK: - IBActions
     @IBAction private func didTapCloseButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ArticleWebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        showLoader()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        hideLoader()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        hideLoader()
     }
 }
 
